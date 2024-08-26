9276b2c0e0c6a0326417e7c8c64cbf74396fccf6
mkas3 <madTwo.19@yandex.ru>
2024-08-26 17:39:45 +0300

    feat: remove tailwind readable plugin

--- src/tailwind/tailwind.config.ts
+++ src/tailwind/tailwind.config.ts
@@ -2,23 +2,18 @@ import type { ESLintUserConfig } from '../index.js';
 
 import type { TailwindConfig } from './tailwind.type.js';
 
-import { tailwindPlugin, tailwindReadablePlugin } from './tailwind.plugin.js';
+import { tailwindPlugin } from './tailwind.plugin.js';
 
 export const tailwindConfig: TailwindConfig = {
   overrides: {
-    'tailwind/classnames-order': 'off',
+    'tailwind/classnames-order': 'warn',
     'tailwind/enforces-negative-arbitrary-values': 'warn',
     'tailwind/enforces-shorthand': 'warn',
     'tailwind/migration-from-tailwind-2': 'warn',
-    'tailwind/no-arbitrary-value': 'off',
+    'tailwind/no-arbitrary-value': 'warn',
     'tailwind/no-contradicting-classname': 'error',
     'tailwind/no-custom-classname': 'warn',
-    'tailwind/no-unnecessary-arbitrary-value': 'warn',
-
-    'tailwind-readable/multiline': ['warn', { classesPerLine: 0, group: 'newLine', lineBreakStyle: 'windows', printWidth: 120 }],
-    'tailwind-readable/no-duplicate-classes': 'warn',
-    'tailwind-readable/no-unnecessary-whitespace': 'warn',
-    'tailwind-readable/sort-classes': 'warn'
+    'tailwind/no-unnecessary-arbitrary-value': 'warn'
   },
   settings: {
     callees: ['classnames', 'clsx', 'cn'],
@@ -32,8 +27,7 @@ export const getTailwindConfig = (config: TailwindConfig): ESLintUserConfig => (
   files: ['**/*.?([cm])[jt]s?(x)'],
   name: 'mkas3/tailwind',
   plugins: {
-    'tailwind': tailwindPlugin,
-    'tailwind-readable': tailwindReadablePlugin
+    tailwind: tailwindPlugin
   },
   rules: config.overrides,
   ...(config.settings ? { settings: { tailwind: config.settings } } : undefined),
