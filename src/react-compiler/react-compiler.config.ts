22bdc5a755c518ca5a91c72c015489e5dee77a62
mkas3 <madTwo.19@yandex.ru>
2024-11-05 14:10:30 +0300

    fix: react-compiler

--- src/react-compiler/react-compiler.config.ts
+++ src/react-compiler/react-compiler.config.ts
@@ -1,13 +1,13 @@
 import type { ESLintUserConfig } from '../index.js';
 
 // @ts-expect-error has no types
-import * as pluginReactCompiler from 'eslint-plugin-react-compiler';
+import { rules } from 'eslint-plugin-react-compiler';
 
 export const reactCompilerUserConfig: ESLintUserConfig = {
   name: 'mkas3/react-compiler',
   plugins: {
     'react-compiler': {
-      rules: pluginReactCompiler.rules
+      rules
     }
   },
   rules: { 'react-compiler/react-compiler': 'error' }
